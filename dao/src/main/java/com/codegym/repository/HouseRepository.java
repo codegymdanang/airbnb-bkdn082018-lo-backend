b2812b9d7d065625687de832f7a22c1c00470133
maitansang <maitansang.200599@gmail.com>
2019-07-28 09:52:55 +0700

    fresh

--- dao/src/main/java/com/codegym/repository/HouseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.repository;
-
-import com.codegym.entity.House;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface HouseRepository extends PagingAndSortingRepository<House,Long> {
-}
